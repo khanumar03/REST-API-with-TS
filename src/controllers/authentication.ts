253adcf6e9caa880fde75912d75d78efe3d62fbc
khan umar <khanumar28122003@gmail.com>
2023-07-23 00:08:09 +0530

    update

--- src/controllers/authentication.ts
+++ src/controllers/authentication.ts
@@ -4,51 +4,63 @@ import { random, authentication } from "../helpers";
 
 export const login = async (req: express.Request, res: express.Response) => {
   try {
-    const {email, password} = req.body
+    const { email, password } = req.body;
 
     if (!email || !password) {
-      return res.status(203).json({message: 'All Feilds are Required'});
+      return res.status(203).json({ message: "All Feilds are Required" });
     }
 
-    const user = await getUserByEmail(email).select('+authentication.salt, +authentication.password')
-    if(!user){
-      return res.status(203).json({message: 'User does not exist !'})
+    const user = await getUserByEmail(email).select({
+      authentication: true,
+    }); // CHANGE
+    console.log(user);
+
+    if (!user) {
+      return res.status(203).json({ message: "User does not exist !" });
     }
 
-    const expectedHash = authentication(user.authentication.salt, password)
+    const expectedHash = authentication(user.authentication.salt, password);
+
     if (user.authentication.password != expectedHash) {
-      return res.status(403).json({message: 'Email or Password is wrong !'})
+      return res.status(403).json({ message: "Email or Password is wrong !" });
     }
 
-    const salt = random()
-    user.authentication.sessionToken = authentication(salt, user._id.toString())
-    await user.save()
+    const salt = random();
+    user.authentication.sessionToken = authentication(
+      salt,
+      user._id.toString()
+    );
+    await user.save();
 
-    res.cookie('REST-AUTH', user.authentication.sessionToken, {domain: 'localhost', path: '/'})
-    return res.status(200).json(user)
-
+    res.cookie("REST-AUTH", user.authentication.sessionToken, {
+      domain: "localhost",
+      path: "/",
+    });
+    return res.status(200).json(user);
   } catch (error) {
     console.log(error);
     return res.status(400);
   }
-}
+};
 
 export const register = async (req: express.Request, res: express.Response) => {
   try {
     const { email, password, username } = req.body;
 
     // console.log(email, password, username);
-    
+
     if (!email || !password || !username) {
-      return res.status(203).json({message: 'All Feilds are Required'});
+      return res.status(203).json({ message: "All Feilds are Required" });
     }
 
     const existingUser = await getUserByEmail(email);
     if (existingUser) {
-      return res.status(203).json({message: 'Already Existed'});
+      return res.status(203).json({ message: "Already Existed" });
     }
 
     const salt = random();
+    console.log();
+
     const user = await createUser({
       email,
       username,
@@ -59,10 +71,9 @@ export const register = async (req: express.Request, res: express.Response) => {
     });
 
     console.log(user);
-    
-    return res.status(200).json(user)
+
+    return res.status(200).json(user);
   } catch (error) {
     console.log(error);
-    return res.status(400);
   }
 };
--- src/index.ts
+++ src/index.ts
@@ -1,34 +1,41 @@
-import express from 'express';
-import http from 'http';
-import bodyParser from 'body-parser';
-import cookieParser from 'cookie-parser';
+import express from "express";
+import http from "http";
+import bodyParser from "body-parser";
+import cookieParser from "cookie-parser";
 // import compression from 'compression';
-import cors from 'cors';
-import mongoose from 'mongoose';
-import router from './router'
+import cors from "cors";
+import mongoose from "mongoose";
+import router from "./router";
 
-const app = express()
-const server = http.createServer(app)
+const app = express();
+const server = http.createServer(app);
 
-app.use(cors({
+app.use(
+  cors({
     credentials: true,
-}))
+  })
+);
 
 // app.use(compression())
-app.use(express.json())
-app.use(cookieParser())
+app.use(express.json());
+app.use(cookieParser());
 
-app.use('/', router())
+app.use("/", router());
 
 server.listen(8080, () => {
-    console.log('Server running on http//localhost:8080/');
-})
+  console.log("Server running on http//localhost:8080/");
+});
 
-const MONGO_URL = 'mongodb+srv://gausnoori07:restapiwts@cluster0.xlu1gnq.mongodb.net/?retryWrites=true&w=majority'
+const MONGO_URL =
+  "mongodb+srv://gausnoori07:restapiwts@cluster0.xlu1gnq.mongodb.net/?retryWrites=true&w=majority";
 
 // mongoose.Promise = Promise
-mongoose.connect(MONGO_URL).then(() =>{
-    console.log('Connected');
-}).catch((err) => {console.log(err)
-})
+mongoose
+  .connect(MONGO_URL)
+  .then(() => {
+    console.log("Connected");
+  })
+  .catch((err) => {
+    console.log(err);
+  });
 // mongoose.connection.on('error', (error: Error) => console.log(error))
